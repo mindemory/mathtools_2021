cb8b2da28a2887881e724074fcfae036a9291b2a
Mrugank Dake <68425163+mindemory@users.noreply.github.com>
2021-09-09 18:24:48 -0400

    Q2 in progress
    Done with Q2(c). Parts, d, e, and f are left.

--- hw1_DAKE/Q2.m
+++ hw1_DAKE/Q2.m
@@ -18,8 +18,38 @@ disp(['The length of the hypotenuse i.e. vector c is: ', num2str(hypotenuse_leng
 % what we computed in question 1
 
 %% 2b)
-%
+% (1)
+met1_length = abs(sqrt(c(1)^2 + c(2)^2));
+disp(['The length of the vector c using method 1 is: ', num2str(met1_length)]);
 
-% The length of the hypotenuse using the norm:
-norm_length = norm(c, 2);
-disp(['The length of the vector c using norm is: ', num2str(norm_length)]);
+% (2)
+met2_length = abs(sqrt(sum(c.^2)));
+disp(['The length of the vector c using method 2 is: ', num2str(met2_length)]);
+
+% (3)
+met3_length = abs(sqrt(dot(c, c)));
+disp(['The length of the vector c using method 3 is: ', num2str(met3_length)]);
+
+% (4)
+met4_length = norm(c, 2);
+disp(['The length of the vector c using method 4 is: ', num2str(met4_length)]);
+
+% (4)
+met5_length = abs(sqrt(c * c'));
+disp(['The length of the vector c using method 5 is: ', num2str(met5_length)]);
+
+
+%% 2c)
+% The angle between the two vectors can be computed as the arccos of the
+% ratio of the dot product of two vectors and the product of norms of the
+% two vectors
+theta = acos(dot(c, a) / (norm(c, 2) * norm(a, 2)));
+disp(['The angle between the vectors c and a is: ', num2str(theta)])
+
+
+%% 2d)
+figure;
+plot(a);
+hold on;
+plot(b);
+plot(c);
