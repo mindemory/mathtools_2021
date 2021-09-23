25234d96a86be5fe1c858cf6067d8baca604197f
mindemory <mrugankdake@gmail.com>
2021-09-23 09:02:28 -0400

    6 files have been added for each question of Assignment 1. Q1 is half-way done.

--- hw1_DAKE/Q1.m
+++ hw1_DAKE/Q1.m
@@ -1,4 +1,21 @@
 clear; close all; clc;
 
 %% 
-% Computing the component of vector v along the di
+% The components of vector v along the direction of u_hat and along the 
+% direction perpendicular to u_hat can be computed by running the
+% inner_prod function (for more details, refer to the function).
+
+%%
+% Creating random vectors v1 and u1 using the randn:
+v1 = randn(2, 1);
+u1 = randn(2, 1);
+
+%%
+% Since u1_hat is supposed to be a unit vector, we compute u1_hat by taking
+% diving u1 by the norm of itself
+u1_hat = u1/norm(u1, 2);
+
+%%
+% Next we compute the v1_x, v1_y, and v1_perp_dist using the inner_prod
+% function
+[v1_x, v1_y, v1_perp_dist] = inner_prod(u1_hat, v1)
