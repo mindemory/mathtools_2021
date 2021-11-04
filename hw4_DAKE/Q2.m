802d8585d611748eb3e7f892222f08be4795427a
Mrugank Dake <68425163+mindemory@users.noreply.github.com>
2021-11-04 17:55:28 -0400

    hw4
    Some progress of Q2 and Q4

--- hw4_DAKE/Q2.m
+++ hw4_DAKE/Q2.m
@@ -2,9 +2,14 @@ clear; close all; clc;
 
 %%
 p = rand(10, 1);
-%p = p/sum(p);
+p = p/sum(p);
+F = [sum(p(1:k)) for k = 1:10];
 sum(p)
 
 
-% function samples = randp(p, num)
-% end
+function samples = randp(p, num)
+    samples = []
+    for i = 1:num
+        samples = [samples, 
+    
+end
--- hw4_DAKE/Q4.m
+++ hw4_DAKE/Q4.m
@@ -4,8 +4,17 @@ clear; close all; clc;
 load('experimentData.mat')
 figure();
 histogram(trialConds, 2);
+cond1_index = find(trialConds == 1);
+cond2_index = find(trialConds == 2);
+cond1_data = data(cond1_index, :);
+cond2_data = data(cond2_index, :);
 
 figure();
-scatter3(data(:, 1), data(:, 2), data(:, 3), [], [.5,.5,.5], 'filled')
+scatter3(cond1_data(:, 1), cond1_data(:, 2), cond1_data(:, 3), 'r', ...
+    'filled', 'DisplayName', 'Condition 1')
+hold on;
+scatter3(cond2_data(:, 1), cond2_data(:, 2), cond2_data(:, 3), 'k', ...
+    'filled', 'DisplayName', 'Condition 2')
+legend();
 xlabel('Voxel 1'); ylabel('Voxel 2'); zlabel('Voxel 3');
 colormap jet
